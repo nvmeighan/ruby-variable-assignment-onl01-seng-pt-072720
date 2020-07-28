a2f3ae2ed9963c0b6860c4c8990f42b1f84cf470
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-28 22:43:32 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,2 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
- filter_parameter_logging
- greeting = "Hello World"
- authenticate_or_request_with_http_basic
+greeting = "Hello World"
  
